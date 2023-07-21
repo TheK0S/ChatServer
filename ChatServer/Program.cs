072017c38d32de1ec22235e5fc65584dff555b7a
Konstantin <turchakkonstantin@gmail.com>
2023-07-21 15:03:00 +0300

    finish

--- ChatServer/Program.cs
+++ ChatServer/Program.cs
@@ -1,4 +1,5 @@
 ﻿using ChatClient.Model;
+using Newtonsoft.Json;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Net;
@@ -11,23 +12,12 @@ namespace ChatServer
     {
         static int ID = 1;
         private static ConcurrentDictionary<int, Socket> clients = new ConcurrentDictionary<int, Socket>();
-        static void Connect()
-        {
-
-        }
-
-        static void Disconnect()
-        {
-
-        }
 
         static async Task Main()
         {
-            // Задайте IP-адрес и порт, на котором сервер будет прослушивать подключения
             IPAddress ipAddress = IPAddress.Any;
             int port = 8080;
 
-            // Создайте объект Socket и свяжите его с IP-адресом и портом
             Socket listener = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
             listener.Bind(new IPEndPoint(ipAddress, port));
             listener.Listen(10);
@@ -35,16 +25,12 @@ namespace ChatServer
 
             while (true)
             {
-                // Принимайте входящие подключения асинхронно
                 Socket client = await listener.AcceptAsync();
 
-                // Создайте уникальный идентификатор клиента
                 int clientId = ID++;
 
-                // Добавьте клиента в словарь клиентов
                 clients.TryAdd(clientId, client);
 
-                // Запустите обработку сообщений от клиента в отдельном потоке
                 _ = HandleClientMessagesAsync(clientId);
             }
         }
@@ -53,23 +39,30 @@ namespace ChatServer
         {
             Socket client = clients[clientId];
 
-            // Получите поток для чтения и записи данных
             using NetworkStream stream = new NetworkStream(client);
 
-            // Создайте буфер для чтения данных
             byte[] buffer = new byte[1024];
             int bytesRead;
+            string ClientName = "undefined user";
 
             try
             {
                 while ((bytesRead = await stream.ReadAsync(buffer, 0, buffer.Length)) > 0)
                 {
-                    // Преобразуйте полученные данные в строку
                     string message = Encoding.UTF8.GetString(buffer, 0, bytesRead);
                     Console.WriteLine($"Сообщение от клиента {clientId}: {message}");
 
-                    // Отправьте полученное сообщение всем остальным клиентам
-                    BroadcastMessage(message, client);
+                    Message msg = JsonConvert.DeserializeObject<Message>(message);
+
+                    if (msg == null) continue;
+
+                    if (msg.Text == "подключился к чату")
+                    {
+                        ClientName = msg.Ouner;
+                        BroadcastMessageAddUser(message , client);                        
+                    }                        
+                    else                        
+                        BroadcastMessage(message);
                 }
             }
             catch (IOException ex)
@@ -82,19 +75,18 @@ namespace ChatServer
             }
             finally
             {
-                // Удалите клиента из словаря клиентов
                 clients.TryRemove(clientId, out _);
 
-                // Закройте клиентское подключение
                 client.Close();
 
-                // Отправьте сообщение о выходе клиента всем остальным клиентам
-                string exitMessage = $"Клиент {clientId} покинул чат.";
-                BroadcastMessage(exitMessage, client);
+                Message exitMsg = new Message { Ouner = $"Пользователь {ClientName} покинул чат.", Text = string.Empty, Date = DateTime.Now.ToLongTimeString() };
+                string jsonMsg = JsonConvert.SerializeObject(exitMsg);
+
+                BroadcastMessage(jsonMsg);
             }
         }
 
-        static void BroadcastMessage(string message, Socket clientSocket)
+        static void BroadcastMessage(string message)
         {
             byte[] messageBytes = Encoding.UTF8.GetBytes(message);
 
@@ -106,145 +98,28 @@ namespace ChatServer
                 }
             }
         }
+        static void BroadcastMessageAddUser(string message, Socket ounerSocket)
+        {
+            Message msg = JsonConvert.DeserializeObject<Message>(message);
 
+            if (msg == null) return;
 
-        //static async Task Main()
-        //{
-        //    ServerObject server = new ServerObject();// создаем сервер
-        //    await server.ListenAsync(); // запускаем сервер
-        //}
+            msg.Ouner = $"Пользователь {msg.Ouner} {msg.Text}";
+            msg.Text = string.Empty;
 
-        //class ServerObject
-        //{
-        //    TcpListener tcpListener = new TcpListener(IPAddress.Parse("127.0.0.1"), 8080);
-        //    List<ClientObject> clients = new List<ClientObject>(); // все подключения
+            message = JsonConvert.SerializeObject(msg);
 
-        //    protected internal void RemoveConnection(string id)
-        //    {
+            if(message == null) return;
 
-        //        ClientObject? client = clients.FirstOrDefault(c => c.Id == id);
+            byte[] messageBytes = Encoding.UTF8.GetBytes(message);
 
-        //        if (client != null) clients.Remove(client);
-        //        client?.Close();
-        //    }
-        //    // прослушивание входящих подключений
-        //    protected internal async Task ListenAsync()
-        //    {
-        //        try
-        //        {
-        //            tcpListener.Start();
-        //            Console.WriteLine("Сервер запущен. Ожидание подключений...");
-
-        //            while (true)
-        //            {
-        //                TcpClient tcpClient = await tcpListener.AcceptTcpClientAsync();
-
-        //                ClientObject clientObject = new ClientObject(tcpClient, this);
-        //                clients.Add(clientObject);
-        //                Task.Run(clientObject.ProcessAsync);
-        //            }
-        //        }
-        //        catch (Exception ex)
-        //        {
-        //            Console.WriteLine(ex.Message);
-        //        }
-        //        finally
-        //        {
-        //            Disconnect();
-        //        }
-        //    }
-
-        //    // трансляция сообщения подключенным клиентам
-        //    protected internal async Task BroadcastMessageAsync(string message, string id)
-        //    {
-        //        foreach (var client in clients)
-        //        {
-        //            if (client.Id != id) // если id клиента не равно id отправителя
-        //            {
-        //                await client.Writer.WriteLineAsync(message); //передача данных
-        //                await client.Writer.FlushAsync();
-        //            }
-        //        }
-        //    }
-        //    // отключение всех клиентов
-        //    protected internal void Disconnect()
-        //    {
-        //        foreach (var client in clients)
-        //        {
-        //            client.Close(); //отключение клиента
-        //        }
-        //        tcpListener.Stop(); //остановка сервера
-        //    }
-        //}
-        //class ClientObject
-        //{
-        //    protected internal string Id { get; } = Guid.NewGuid().ToString();
-        //    protected internal StreamWriter Writer { get; }
-        //    protected internal StreamReader Reader { get; }
-
-        //    TcpClient client;
-        //    ServerObject server;
-
-        //    public ClientObject(TcpClient tcpClient, ServerObject serverObject)
-        //    {
-        //        client = tcpClient;
-        //        server = serverObject;
-        //        // получаем NetworkStream для взаимодействия с сервером
-        //        var stream = client.GetStream();
-        //        // создаем StreamReader для чтения данных
-        //        Reader = new StreamReader(stream);
-        //        // создаем StreamWriter для отправки данных
-        //        Writer = new StreamWriter(stream);
-        //    }
-
-        //    public async Task ProcessAsync()
-        //    {
-        //        try
-        //        {
-        //            // получаем имя пользователя
-        //            string? userName = await Reader.ReadLineAsync();
-        //            string? message = $"{userName} вошел в чат";
-        //            // посылаем сообщение о входе в чат всем подключенным пользователям
-        //            await server.BroadcastMessageAsync(message, Id);
-        //            Console.WriteLine(message);
-        //            // в бесконечном цикле получаем сообщения от клиента
-        //            while (true)
-        //            {
-        //                try
-        //                {
-        //                    message = await Reader.ReadLineAsync();
-        //                    if (message == null) continue;
-        //                    message = $"{userName}: {message}";
-        //                    Console.WriteLine(message);
-        //                    await server.BroadcastMessageAsync(message, Id);
-        //                }
-        //                catch
-        //                {
-        //                    message = $"{userName} покинул чат";
-        //                    Console.WriteLine(message);
-        //                    await server.BroadcastMessageAsync(message, Id);
-        //                    break;
-        //                }
-        //            }
-        //        }
-        //        catch (Exception e)
-        //        {
-        //            Console.WriteLine(e.Message);
-        //        }
-        //        finally
-        //        {
-        //            // в случае выхода из цикла закрываем ресурсы
-        //            server.RemoveConnection(Id);
-        //        }
-        //    }
-        //    // закрытие подключения
-        //    protected internal void Close()
-        //    {
-        //        Writer.Close();
-        //        Reader.Close();
-        //        client.Close();
-        //    }
-        //}
+            foreach (var client in clients.Values)
+            {
+                if (client.Connected && client != ounerSocket)
+                {
+                    client.Send(messageBytes);
+                }
+            }
+        }
     }
-
 }
